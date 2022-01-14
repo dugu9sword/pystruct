5e16f6cb2e2782c90b00be2960f39aef43f6454b
zhouyi <dugu9sword@163.com>
2022-01-14 16:38:37 +0800

    add comment to max-prod

--- pystruct/inference/maxprod.py
+++ pystruct/inference/maxprod.py
@@ -32,7 +32,8 @@ def inference_max_product(unary_potentials, pairwise_potentials, edges,
         Unary potentials of energy function.
 
     pairwise_potentials : nd-array
-        Pairwise potentials of energy function.
+        Pairwise potentials of energy function. 
+        Shape: (n_states, n_states) or (n_edges, n_states, n_states)
 
     edges : nd-array
         Edges of energy function.
@@ -63,6 +64,19 @@ def inference_max_product(unary_potentials, pairwise_potentials, edges,
 
 
 def tree_max_product(unary_potentials, pairwise_potentials, edges):
+    """
+        Parameters
+        ----------
+        unary_potentials:    (n_vertices, n_states)
+        pairwise_potentials: (n_vertices, n_states, n_states)
+        edges:               (n_vertices, 2)
+        
+        @comment: Yi
+        forward  -> outside
+        backward -> inside
+        See https://www.cs.jhu.edu/~jason/papers/eisner.spnlp16.pdf
+        
+    """
     n_vertices, n_states = unary_potentials.shape
     parents = -np.ones(n_vertices, dtype=np.int)
     visited = np.zeros(n_vertices, dtype=np.bool)
@@ -78,6 +92,36 @@ def tree_max_product(unary_potentials, pairwise_potentials, edges):
     messages_backward = np.zeros((n_vertices, n_states))
     pw_forward = np.zeros((n_vertices, n_states, n_states))
     # build a breadth first search of the tree
+    """
+        @comment: Yi
+        Given a tree with 11 vertices and 3 states:
+
+                3
+              /   \ 
+             9     5
+            / \     \
+           0   4     2
+         / | \      / \
+        8  7  6    1   10
+             
+        BFS: [0, 9, 8, 7, 6, 3, 4, 5, 2, 1, 10]
+        Decide the root: Every node in a tree can be seen as the root node 
+        of the subtree rooted at that node. Each neighbour of the root node 
+        is viewed as its child.
+        https://en.wikipedia.org/wiki/Tree_(data_structure)
+        
+        Important notations:
+        neighbors & pairwise_weights:
+                                            | h1-c1 h1-c2 h1-c3 |
+                pairwise_weights[h][c]  =   | h2-c1 h2-c2 h2-c3 |
+                                            | h3-c1 h3-c2 h3-c3 |
+        pw_forward:
+            when traversing the tree, a neighbor is always a child of the node.
+            pw_forward[neighbor]'s row denotes its parent.
+                                | h1-c1 h1-c2 h1-c3 |
+                pw_forward[c] = | h2-c1 h2-c2 h2-c3 |
+                                | h3-c1 h3-c2 h3-c3 |
+    """
     traversal = []
     lonely = 0
     while lonely < n_vertices:
@@ -102,6 +146,18 @@ def tree_max_product(unary_potentials, pairwise_potentials, edges):
                 elif not parents[node] == neighbor:
                     raise ValueError("Graph not a tree")
     # messages from leaves to root
+    """
+        @comment: Yi
+        messages_backward saves the message from its descedents. 
+        
+        In numpy, (3, ) is not row/column vector, but just a 1-d array.
+        (3, ) + (n, 3) is broadcasted as (n, 3) + (n, 3)
+        
+        msg_bw     +     unary    +       pw_forward
+                                    | h1-c1 h1-c2 h1-c3 |
+        |c1 c2 c3| + | c1 c2 c3 | + | h2-c1 h2-c2 h2-c3 | = similar to pw_forward
+                                    | h3-c1 h3-c2 h3-c3 | 
+    """
     for node in traversal[::-1]:
         parent = parents[node]
         if parent != -1:
@@ -110,6 +166,15 @@ def tree_max_product(unary_potentials, pairwise_potentials, edges):
             message -= message.max()
             messages_backward[parent] += message
     # messages from root back to leaves
+    """
+        @comment: Yi
+        messages_forward saves the message from its ancestor. 
+        
+        msg_fw     +     unary    +       pw_forward.T
+                                    | c1-h1 c1-h2 c1-h3 |
+        |h1 h2 h3| + | h1 h2 h3 | + | c2-h1 c2-h2 c2-h3 | = ...
+                                    | c3-h1 c3-h2 c3-h3 | 
+    """
     for node in traversal:
         parent = parents[node]
         if parent != -1:
@@ -127,6 +192,30 @@ def tree_max_product(unary_potentials, pairwise_potentials, edges):
 
 def iterative_max_product(unary_potentials, pairwise_potentials, edges,
                           max_iter=10, damping=.5, tol=1e-5):
+    """
+        Parameters
+        ----------
+        unary_potentials:    (n_vertices, n_states)
+        pairwise_potentials: (n_vertices, n_states, n_states)
+        edges:               (n_vertices, 2)
+        
+        @comment: Yi
+        For an edge connecting two nodes 0 and 1, messages[e, 0] denotes the last message 
+        sent from 0 to 1. 
+        
+        Firstly, in a new update, the new 0->1 message (messages[0]) is:
+        - 0's all incoming messages except 1:
+            +  all_incoming[0]
+            -  messages[1] (messages[1] is used to compute message[0], and vice versa)
+        - potentials:
+            + 0's unary potential
+            + (0, 1)'s pairwise potential
+        
+        Then, we need to update:
+        - the 0->1 message (messages[0]) by damping (See MLAPP)
+        - the incoming messages for 1 (all_incoming[1])
+        
+    """
     n_edges = len(edges)
     n_vertices, n_states = unary_potentials.shape
     messages = np.zeros((n_edges, 2, n_states))
@@ -135,9 +224,9 @@ def iterative_max_product(unary_potentials, pairwise_potentials, edges,
         diff = 0
         for e, (edge, pairwise) in enumerate(zip(edges, pairwise_potentials)):
             # update message from edge[0] to edge[1]
-            update = (all_incoming[edge[0]] + pairwise.T +
-                      unary_potentials[edge[0]]
-                      - messages[e, 1])
+            update = (all_incoming[edge[0]] - messages[e, 1]
+                      + unary_potentials[edge[0]]
+                      + pairwise.T)
             old_message = messages[e, 0].copy()
             new_message = np.max(update, axis=1)
             new_message -= np.max(new_message)
@@ -148,9 +237,9 @@ def iterative_max_product(unary_potentials, pairwise_potentials, edges,
             diff += np.abs(update).sum()
 
             # update message from edge[1] to edge[0]
-            update = (all_incoming[edge[1]] + pairwise +
-                      unary_potentials[edge[1]]
-                      - messages[e, 0])
+            update = (all_incoming[edge[1]] - messages[e, 0]
+                      + unary_potentials[edge[1]]
+                      + pairwise)
             old_message = messages[e, 1].copy()
             new_message = np.max(update, axis=1)
             new_message -= np.max(messages[e, 1])
